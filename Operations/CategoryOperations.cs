ad08425b816d5f2407bddc05d780ab214ea6294e
Rasmus Dupont Søgaard <rasmusduponts@gmail.com>
2017-10-20 13:04:03 +0200

    refactor

---
+++ Operations/CategoryOperations.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assignment4.Operations
+{
+    class CategoryOperations
+    {
+        //9. Get category by ID
+        public static dynamic GetCategory(NorthwindContext db, int id)
+        {
+            var category = db.Categories
+                .Where(x => x.Id == id);
+            //.Select(c => c);
+
+            return category;
+        }
+
+        //10. Get all categories
+        public static dynamic GetAllCategories(NorthwindContext db)
+        {
+            var categories = db.Categories.Select(x => new { x.Id, x.Name, x.Description });
+
+            return categories;
+        }
+
+        //11. Add new category
+        public static dynamic AddCategory(NorthwindContext db, string name, string description)
+        {
+            var category = new Category { Name = "new object", Description = "This is new" };
+            db.Categories.Add(category);
+            db.SaveChanges();
+
+            return category;
+        }
+
+        //12. Update category
+        public static bool UpdateCategory(NorthwindContext db, int id, string name)
+        {
+            try
+            {
+                var category = db.Categories.FirstOrDefault(x => x.Id == 11);
+                if (category != null) category.Name = name;
+                db.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        //13. Delete category
+        public static bool DeleteCategory(NorthwindContext db, int id)
+        {
+            try
+            {
+                var category = db.Categories.FirstOrDefault(x => x.Id == id);
+                db.Categories.Remove(category);
+                db.SaveChanges();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Operations/ProductOperations.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assignment4.Operations
+{
+    class ProductOperations
+    {
+        //6. Get a single product by ID
+        public dynamic GetProduct(NorthwindContext db, int id)
+        {
+            var product = db.Products
+                .Where(x => x.Id == id)
+                .Include(oD => oD.OrderDetails)
+                .Select(p => new { p.OrderDetails.Quantity, p.OrderDetails.Discount, p.Name, p.UnitPrice, CategoryName = p.Category.Name });
+
+            return product;
+        }
+
+        //7. Get a list of products that contains a substring
+        public dynamic GetProductsBySubstring(NorthwindContext db, string sub)
+        {
+            var products = db.Products
+                .Where(x => x.Name.Contains(sub))
+                .Include(c => c.Category)
+                .Select(p => new { p.Name, CategoryName = p.Category.Name });
+
+            return products;
+        }
+
+        //8. Get products by category ID
+        public dynamic GetProductsByCategoryId(NorthwindContext db, int id)
+        {
+            var products = db.Products
+                .Where(x => x.Category.Id == id)
+                .Select(p => new { p.Name, CategoryName = p.Category.Name });
+
+            return products;
+        }
+    }
+}
